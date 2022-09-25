e71597c610684bd00404d0b16f6ab56b52066e72
DiggsAsura <kenneth.bjerke@gmail.com>
2022-09-25 23:51:31 +0200

    what_to_do.cpp

---
+++ codecademy/Cpp/tmp/what_to_do.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	std::cout << "To mac or deliver it back??? \n";
+	std::cout << "Not that I don't like it a lot, \n";
+	std::cout << "it's an amazing machine. However - \n";
+	std::cout << "it feels annoying to do C++ on it... \n";
+	std::cout << "to the point where I really miss linux \n";
+}
