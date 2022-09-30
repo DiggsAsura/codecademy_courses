8e76eeb92ed8440496f9efb0c433103ec355d230
DiggsAsura <kenneth.bjerke@gmail.com>
2022-09-30 23:13:41 +0200

    blocks cleaned up

--- codecademy/rust_beta/a5_scope_and_ownership/target/debug/a5_scope_and_ownership.d
+++
@@ -1,1 +0,0 @@
-/Users/kenneth/github/study/codecademy/rust_beta/a5_scope_and_ownership/target/debug/a5_scope_and_ownership: /Users/kenneth/github/study/codecademy/rust_beta/a5_scope_and_ownership/src/main.rs
--- codecademy/rust_beta/a5_scope_and_ownership/target/debug/deps/a5_scope_and_ownership-c7b79f036cfffab9.d
+++
@@ -1,5 +0,0 @@
-/Users/kenneth/github/study/codecademy/rust_beta/a5_scope_and_ownership/target/debug/deps/a5_scope_and_ownership-c7b79f036cfffab9: src/main.rs
-
-/Users/kenneth/github/study/codecademy/rust_beta/a5_scope_and_ownership/target/debug/deps/a5_scope_and_ownership-c7b79f036cfffab9.d: src/main.rs
-
-src/main.rs:
