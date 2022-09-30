b9cdb367a3b3019fc6e0322cb3c1dd400a00f6c4
DiggsAsura <kenneth.bjerke@gmail.com>
2022-09-30 23:14:05 +0200

    tmp

---
+++ codecademy/rust_beta/tmp/readme.md
@@ -0,0 +1,4 @@
+### tmp
+
+Just random code snippets, usually tests. Either related to anything or not. 
+Usually not gonna explain anything about these. 
---
+++ codecademy/rust_beta/tmp/test_blocks_scope.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Hmm this does not seem to work
+    let sum = {
+        number_1 = 12;
+        number_2 = 23;
+        number_1 + number_2
+    };
+    println!("{sum}");
+}
