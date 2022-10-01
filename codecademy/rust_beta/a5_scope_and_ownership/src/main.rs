30df306a2efea847c417103441eae4814130f9c4
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-01 16:54:59 +0200

    rust beta walkthrough continue

--- codecademy/rust_beta/a5_scope_and_ownership/src/main.rs
+++ codecademy/rust_beta/a5_scope_and_ownership/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
 
+    println!("{:?}", struct_test());
+//    println!("{:?}", visibility());
+    println!("{:?}", scope());
     println!("{:?}", p1_blocks());
     println!("{:?}", sum());    
 
@@ -51,5 +54,87 @@ fn sum() -> i32 {
     let number_1 = -11;
     let number_2 = -12;
     number_1 + number_2
+}
+
+
+// SCOPE
+// -------
+
+// Scope is the concept of wheter or not a particular item exists in memory
+// and is accessible at a certain location in our codebase. In Rust, the scope
+// of any particular item is limited to the block it is contained in.
+//
+// When a block is closed, all of its values are released from memory and
+// are then considered out-of-scope. If an item is accessible, it is considered
+// in-scope.
+//
+// The following example showcases how scoping works by shadowing a variable.
+// Note how our last println!() call will print "10" because our second 
+// declaration of a number is dropped from memory at the end of its containing
+// block.
+//
+
+fn scope() {
+    let number = 10;
+    {
+        println!("{number}"); // Prints 10
+        
+        let number = 22;
+        println!("{number}"); // Prints 22
+    }
+    // Our second declaration of 'number' is dropped from memory after }. 
+    // It is now considered out of scope.
+
+    println!("{number}"); // Prints 10
+}
+
+// VISIBILITY
+// -----------
+//
+// We can make an item accessible outside of its normal scope by denoting it as
+// public with the pub keyword.
+//
+// All items in Rust are private by default. Private items can only be accessed
+// within their declared module and any children modules. 
+//
+
+//fn visibility() {
+//    mod numbers {
+//        pub const ZERO: i32 = 0;
+//    }
+//
+//    mod another_scope {
+//        use super::numbers::ZERO;
+//
+//        fn print_zero() {
+//            println!("{ZERO}");
+//        }
+//    }
+//}
+//
+// (above does not work written like this)
+//
+
+//fn visibility() {
+//    {
+//        pub const ZERO: i32 = 0;
+//    }
+//
+//    println!("{ZERO}");
+//}
+
+// above does not work either hm. 
+//
+
+fn struct_test() {
+    pub struct Number {
+        pub value: i32,
+    }
+
+    let mut number = Number { value: 0 };
+
+    number.value += 1;
+    println!("{}", number.value);
+    println!("End of struct_test()");
 }
 
---
+++ codecademy/rust_beta/a5_scope_and_ownership/target/debug/deps/a5_scope_and_ownership-c7b79f036cfffab9.d
@@ -0,0 +1,5 @@
+/Users/kenneth/github/study/codecademy/rust_beta/a5_scope_and_ownership/target/debug/deps/a5_scope_and_ownership-c7b79f036cfffab9: src/main.rs
+
+/Users/kenneth/github/study/codecademy/rust_beta/a5_scope_and_ownership/target/debug/deps/a5_scope_and_ownership-c7b79f036cfffab9.d: src/main.rs
+
+src/main.rs:
