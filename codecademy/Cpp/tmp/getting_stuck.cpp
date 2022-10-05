604f4aeab2f03b011c27a08a5ff041d3183b4fd1
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-05 22:12:28 +0200

    getting_stuck.cpp

---
+++ codecademy/Cpp/tmp/getting_stuck.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "The Tic Tac Toe throw me off for a couple days\n";
+    std::cout << "I was getting somewhere...\n";
+    std::cout << "But then they release the Rust beta course, which I \n";
+    std::cout << "really have been waiting for.\n\n";
+    std::cout << "Now a bit hard getting back here. Hmm How!\n";
+    std::cout << "Is there another answer then 42?\n\n";
+}
