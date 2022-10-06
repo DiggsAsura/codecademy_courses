e3f7e7df90172a2c970f2883582d9df4ce17b167
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-06 22:31:34 +0200

    22_oct_6.cpp

---
+++ did_not_do_anything_today_fml/22_oct_6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Ok let's not trick anyone.\n";
+    std::cout << "This way i keep my streak, and come clean at the\n";
+    std::cout << "same time... lol. \n\n";
+
+    std::cout << "So yea, i keep the streak to not loose motivation. It\n";
+    std::cout << "keeps me going. Even if I have days I don't progress.\n\n";
+
+    std::cout << "It's really not for the streak alone. It's just a tool\n";
+    std::cout << "to NOT make me loose focus for any super extended time\n";
+    std::cout << "period.\n\n";
+
+    std::cout << "I fear I WILL stop for longer and longer periodes if I \n";
+    std::cout << "don't keep uploading something, even if it's just b.s \n";
+    std::cout << "like this.\n\n";
+
+    std::cout << "To my defense, I also work through Cisco Networking\n";
+    std::cout << "Basics course at school, and cathing up with other \n";
+    std::cout << "subjects. And clean home lol. I'll be back, you hear me??\n\n";
+    std::cout << "cheers, Diggs\n\n";
+}
+
