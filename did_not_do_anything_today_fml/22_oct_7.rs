a527eb88ff9f584365556b84cf4289e6e0cf0f1b
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-07 21:59:45 +0200

    sorry but not sorry

---
+++ did_not_do_anything_today_fml/22_oct_7.rs
@@ -0,0 +1,16 @@
+fn main() {
+    excuse();
+}
+
+fn excuse() {
+    println! {"
+    Again, catching up with school and getting family back home from
+    their vacay. I did not go, literally to catch up with math, 
+    networking (cisco certificate) and other school related stuff.
+    Another day without hard focus on coding, but I always keep it in
+    the back of my head!
+
+    cheers Diggs
+    "}
+}
+
