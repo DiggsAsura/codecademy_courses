e1a140fe5cc988f455ba1b9252807b11dfe69c51
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-10 16:28:37 +0200

    naming convention

---
+++ hello_rust/rust_by_example/b-0-primitives/target/debug/deps/b1_primitives-218fc5108bb60e1a.d
@@ -0,0 +1,5 @@
+/Users/kenneth/github/study/hello_rust/rust_by_example/b-0-primitives/target/debug/deps/b1_primitives-218fc5108bb60e1a.rmeta: src/main.rs
+
+/Users/kenneth/github/study/hello_rust/rust_by_example/b-0-primitives/target/debug/deps/b1_primitives-218fc5108bb60e1a.d: src/main.rs
+
+src/main.rs:
