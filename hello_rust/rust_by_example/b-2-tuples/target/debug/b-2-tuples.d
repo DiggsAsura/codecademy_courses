48ac20134228bc0a45a704e41d9b7a19080207ce
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-11 15:07:55 +0200

    tuples

---
+++ hello_rust/rust_by_example/b-2-tuples/target/debug/b-2-tuples.d
@@ -0,0 +1,1 @@
+/Users/kenneth/github/study/hello_rust/rust_by_example/b-2-tuples/target/debug/b-2-tuples: /Users/kenneth/github/study/hello_rust/rust_by_example/b-2-tuples/src/main.rs
---
+++ hello_rust/rust_by_example/b-2-tuples/target/debug/deps/b_2_tuples-f3cb4f94c8ffd351.d
@@ -0,0 +1,5 @@
+/Users/kenneth/github/study/hello_rust/rust_by_example/b-2-tuples/target/debug/deps/b_2_tuples-f3cb4f94c8ffd351.rmeta: src/main.rs
+
+/Users/kenneth/github/study/hello_rust/rust_by_example/b-2-tuples/target/debug/deps/b_2_tuples-f3cb4f94c8ffd351.d: src/main.rs
+
+src/main.rs:
