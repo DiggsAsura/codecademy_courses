d12e6409f1cd0cbccc19001252861f4ae6306317
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-21 08:36:22 +0200

    cleaning up cargo clean

--- hello_rust/selfstudy/structs/target/debug/deps/structs_intestigation-7f9174117de4af69.d
+++
@@ -1,5 +0,0 @@
-/Users/kenneth/github/study/hello_rust/selfstudy/structs/target/debug/deps/structs_intestigation-7f9174117de4af69.rmeta: src/main.rs
-
-/Users/kenneth/github/study/hello_rust/selfstudy/structs/target/debug/deps/structs_intestigation-7f9174117de4af69.d: src/main.rs
-
-src/main.rs:
--- hello_rust/selfstudy/structs/target/debug/deps/structs_intestigation-8983c988aa119447.d
+++
@@ -1,5 +0,0 @@
-/Users/kenneth/github/study/hello_rust/selfstudy/structs/target/debug/deps/structs_intestigation-8983c988aa119447.rmeta: src/main.rs
-
-/Users/kenneth/github/study/hello_rust/selfstudy/structs/target/debug/deps/structs_intestigation-8983c988aa119447.d: src/main.rs
-
-src/main.rs:
