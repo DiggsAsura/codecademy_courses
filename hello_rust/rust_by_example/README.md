4cfb4116328c249934b91049849ab20bdeabcec2
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-23 17:24:27 +0200

    naming

--- hello_rust/rust_by_example/README.md
+++
@@ -1,6 +0,0 @@
-# Rust by Example
-
-Going to go through the examples on the official webpage. 
-https://doc.rust-lang.org/rust-by-example/index.html
-
-Sun Oct 9 2022
--- hello_rust/rust_by_example/b-0-primitives/target/debug/deps/b1_primitives-6a246f1e6d2e7b51.d
+++
@@ -1,5 +0,0 @@
-/Users/kenneth/github/study/hello_rust/rust_by_example/b-0-primitives/target/debug/deps/b1_primitives-6a246f1e6d2e7b51.rmeta: src/main.rs
-
-/Users/kenneth/github/study/hello_rust/rust_by_example/b-0-primitives/target/debug/deps/b1_primitives-6a246f1e6d2e7b51.d: src/main.rs
-
-src/main.rs:
