8c64d2c189b2ca1cdf5e5b078f1d2fcae2f6a37c
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-23 17:59:20 +0200

    e-0-types/

---
+++ hello_rust/02_rust_by_example/e-0-types/src/main.rs
@@ -0,0 +1,15 @@
+/* Types
+ * =====
+ *
+ * Rust provides several mechanisms to change or define the type of primitive and user defined
+ * types. The following section cover:
+ *
+ *  - Casting between primitive types
+ *  - Specifying the desired type of literals
+ *  - Using type type interfence 
+ *  - Aliasing types 
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ hello_rust/02_rust_by_example/e-0-types/target/debug/deps/e_0_types-9d08260db4da44be.d
@@ -0,0 +1,5 @@
+/Users/kenneth/github/study/hello_rust/02_rust_by_example/e-0-types/target/debug/deps/e_0_types-9d08260db4da44be.rmeta: src/main.rs
+
+/Users/kenneth/github/study/hello_rust/02_rust_by_example/e-0-types/target/debug/deps/e_0_types-9d08260db4da44be.d: src/main.rs
+
+src/main.rs:
