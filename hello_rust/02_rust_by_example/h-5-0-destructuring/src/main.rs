5d9fa3231f21bc88b7ec2a8f2390f2b7a2e1407c
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-24 18:11:29 +0200

    h-5-0-destructuring

---
+++ hello_rust/02_rust_by_example/h-5-0-destructuring/src/main.rs
@@ -0,0 +1,14 @@
+/* Destructuring 
+ *
+ * A match block can destructure items in a variety of ways.
+ *
+ *  - Destructuring Tuples
+ *  - Destructuring Arrays and Slices
+ *  - Destructuring Enums
+ *  - Destructuring Pointers
+ *  - Destructuring Structures
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
