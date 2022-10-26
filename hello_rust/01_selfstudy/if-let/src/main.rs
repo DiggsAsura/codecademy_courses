e98972cb01a54c28e677c62b8ff2ac8edbedd181
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-26 14:49:37 +0200

    if-let/

---
+++ hello_rust/01_selfstudy/if-let/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let name = "kenneth";
+
+    if let "kenneth" = name {
+        println!("{name}");
+    }
+    let number = Some(9);
+
+    if let Some(i) = number {
+        println!("{i}");
+    }
+
+    /* I understand but don't understande i guess. */
+}
