04fda71746f7f4a556b1411484e2953b9341b746
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-27 23:05:47 +0200

    i-2-4-input-functions/

---
+++ hello_rust/02_rust_by_example/i-2-4-input-functions/src/main.rs
@@ -0,0 +1,32 @@
+/* Input functions
+ * ================
+ *
+ * Since closures may be used as arguments, you might wonder if the same can be said about
+ * functions. And indeed they can! If you declare a function that takes a closure as parameter,
+ * then any function that satisfies the trait bound of that closure can be passed as a parameter.*/
+
+// Define a function which makes a generic 'F' argument
+// bounded by 'Fn', and calls it
+fn call_me<F: Fn()>(f: F) {
+    f();
+}
+
+// Define a wrapper function satisfying the 'Fn' bound
+fn function() {
+    println!("I'm a function!!");
+}
+
+fn main() {
+    // Define a closure satisfying the 'Fn' bound
+    let closure = || println!("I'm a closure!");
+
+    call_me(closure);
+    call_me(function);
+}
+
+/* As an additional note, the Fn, FnMut, and FnOnce traits dictate how a closure captures variables
+ * from the enclosing scope. */
+
+        // Understand but not really understand. I feel i get a better understanding of F, at
+        // least. 
+
