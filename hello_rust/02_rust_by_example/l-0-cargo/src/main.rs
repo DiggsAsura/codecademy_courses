ae2a8dbbb8ce7dc540ceccaee57145e6572b9f7f
DiggsAsura <kenneth.bjerke@gmail.com>
2022-10-30 20:50:50 +0100

    l-0-cargo/

---
+++ hello_rust/02_rust_by_example/l-0-cargo/src/main.rs
@@ -0,0 +1,15 @@
+/* Cargo
+ * ======
+ *
+ * cargo is the official Rust package management tool. It has lots of really useful features to
+ * improve code quality and developer velocity! These include
+ *
+ *  - Dependency management and integration with crates.io (the official Rust package registry)
+ *  - Awarness of unit tests
+ *  - Awarness of benchmarks
+ *
+ * This chapter will go through some quick basics, but you can find the comprehensive docs in the
+ * Rust Book. */
+fn main() {
+    println!("Hello, world!");
+}
