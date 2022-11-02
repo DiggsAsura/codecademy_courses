616fcbf5cb11bb56d9d1906d1db2b3cc789f0858
DiggsAsura <kenneth.bjerke@gmail.com>
2022-11-02 22:46:40 +0100

    n-8-0-associated-items/

---
+++ hello_rust/02_rust_by_example/n-8-0-associated-items/src/main.rs
@@ -0,0 +1,11 @@
+/* Associated items
+ * =================
+ *
+ * "Associated Items" refers to a set of rules pertainint to itmes of various types. It is an
+ * extension to trait generics, and allows traits to internally define new items. 
+ *
+ * One such item is called an associated type, providing simpler usage patterns when the trait is
+ * generic over its container type. */
+fn main() {
+    println!("Hello, world!");
+}
