3e23856706c3f7c17ca03db13d3244855e4b4bef
Kenneth <kenneth.bjerke@gmail.com>
2022-11-12 14:45:34 +0100

    q-1-0-syntax/

---
+++ hello_rust/02_rust_by_example/q-1-0-syntax/src/main.rs
@@ -0,0 +1,14 @@
+/* Syntax
+ * ========
+ *
+ * In following subsections, we will show how to define macros in Rust. There are three basic
+ * ideas:
+ *
+ *      1. Patterns and Designators
+ *      2. Overloading
+ *      3. Repetition
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
