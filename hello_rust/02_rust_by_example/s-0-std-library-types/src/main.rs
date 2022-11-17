7294fb28bce2c67b802c983d3520111a002e58b3
Kenneth <kenneth.bjerke@gmail.com>
2022-11-17 21:03:38 +0100

    s-0-std-library-types/

---
+++ hello_rust/02_rust_by_example/s-0-std-library-types/src/main.rs
@@ -0,0 +1,16 @@
+/* Std library types
+ * =================
+ *
+ * The std library provides many custom types which expands drastically on the primitives.
+ * Some of these include:
+ *
+ *  - growable Strings like: "hello world"
+ *  - growable vectors: [1, 2, 3]
+ *  - optional types: Option<i32>
+ *  - error handling types: Result<i32, i32>
+ *  - heap allocated pointers: Box<i32>
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
