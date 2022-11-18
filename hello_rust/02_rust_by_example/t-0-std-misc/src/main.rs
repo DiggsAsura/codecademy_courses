1ebd01393cd368bd06a8ab78bce945ad1e5033a2
Kenneth <kenneth.bjerke@gmail.com>
2022-11-18 23:46:54 +0100

    t-0-std-misc/

---
+++ hello_rust/02_rust_by_example/t-0-std-misc/src/main.rs
@@ -0,0 +1,13 @@
+/* Std misc
+ * =========
+ *
+ * Many other types are provided by the std library to support things such as:
+ *
+ * - Threads
+ * - Channels
+ * - File I/O
+ *
+ * These expand beyond what the primitives provide. */
+fn main() {
+    println!("Hello, world!");
+}
