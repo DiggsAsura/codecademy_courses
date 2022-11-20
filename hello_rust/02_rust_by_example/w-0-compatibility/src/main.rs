35c66909f40c349a4729ba97e2c934c592249508
Kenneth <kenneth.bjerke@gmail.com>
2022-11-20 21:05:56 +0100

    w-0-compatibility/

---
+++ hello_rust/02_rust_by_example/w-0-compatibility/src/main.rs
@@ -0,0 +1,13 @@
+/* Compatibility 
+ * ==============
+ *
+ * The Rust language is fastly evolving, and because of this certain compatibility issues can
+ * arise, despite efforts to ensure forwards-compatibility wherever possible.
+ *
+ *  - Raw identifiers
+ *
+ * Read on..*/
+
+fn main() {
+    println!("Hello, world!");
+}
