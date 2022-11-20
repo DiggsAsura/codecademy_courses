b9a4124d60413626f2fd1674da720d76ac02ba83
Kenneth <kenneth.bjerke@gmail.com>
2022-11-20 02:01:59 +0100

    u-0-testing/

---
+++ hello_rust/02_rust_by_example/u-0-testing/src/main.rs
@@ -0,0 +1,26 @@
+/* Testing
+ * ========
+ *
+ * Rust is a programming language that cares a lot about correctness and it includes support for
+ * writing software tests within the language itself.
+ *
+ * Testing comes in three styles:
+ *
+ *  - Unit testing
+ *  - Doc testing
+ *  - Integration testing.
+ *
+ * Also Rust has support for specifying additional dependencies for tests:
+ *
+ *  - Dev-dependencies
+ *
+ *
+ * See Also
+ *
+ *  - The Book chapter on testing
+ *  - API Guidelines on doc-testing
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
