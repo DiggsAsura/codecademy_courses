0bb524a83b795971ef0db435717ae517551b8245
Kenneth <kenneth.bjerke@gmail.com>
2022-11-24 21:48:32 +0100

    numeric-operations/

---
+++ hello_rust/00-experiment-the-rust-book/3-common-programming-concepts/2-data-types/numeric-operations/src/main.rs
@@ -0,0 +1,21 @@
+/* Numeric Operations
+ *
+ * Rust supports the basic stuff you'd expect. */
+
+fn main() {
+    // addition
+    let sum = 4 + 3;
+
+    // subtraction
+    let difference = 95.4 - 23.3;
+
+    // multiplication
+    let product = 3 * 29;
+
+    // division
+    let quotient = 45.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+
+    // remainder / modulo
+    let remainder = 23 % 3;
+}
