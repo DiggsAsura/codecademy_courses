a0f1d56d65292ae7ab9e059a09c0d604a02a1858
Kenneth <kenneth.bjerke@gmail.com>
2022-11-24 22:07:03 +0100

    compound-types/

---
+++ hello_rust/00-experiment-the-rust-book/3-common-programming-concepts/2-data-types/compound-types/src/main.rs
@@ -0,0 +1,11 @@
+/* Compound Types 
+ * ----------------
+ *
+ * Compound types can group multiple values into one type. Rust havs two primitive compound types:
+ *  - Tuples
+ *  - Arrays
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
