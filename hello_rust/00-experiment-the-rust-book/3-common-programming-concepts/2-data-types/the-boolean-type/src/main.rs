1a775f8f9349c3b39a988f97c6abfd691246da65
Kenneth <kenneth.bjerke@gmail.com>
2022-11-24 21:49:47 +0100

    the-boolean-type/

---
+++ hello_rust/00-experiment-the-rust-book/3-common-programming-concepts/2-data-types/the-boolean-type/src/main.rs
@@ -0,0 +1,9 @@
+/* The Boolean Type 
+ *
+ * Booleans are one byte in size. keyword bool. */
+
+fn main() {
+    let t = true;
+    let f: bool = false; // with explicit type annotation
+                         
+}
