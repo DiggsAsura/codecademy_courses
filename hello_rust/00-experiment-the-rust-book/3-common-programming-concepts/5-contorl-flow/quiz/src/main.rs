1a339713659d8469ccb302790dc15be8ae7da25f
Kenneth <kenneth.bjerke@gmail.com>
2022-11-26 14:19:02 +0100

    quiz/

---
+++ hello_rust/00-experiment-the-rust-book/3-common-programming-concepts/5-contorl-flow/quiz/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut x = 0;
+    'a: loop {
+        x += 1;
+        'b: loop {
+            if x > 10 {
+                continue 'a;
+            } else {
+                break 'b;
+            }
+        }
+        break;
+    }
+    println!("{x}");
+}
