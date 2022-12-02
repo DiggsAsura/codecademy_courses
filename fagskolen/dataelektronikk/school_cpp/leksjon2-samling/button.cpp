2dacff4c1ec15fb27583f9405f7199b38958368e
DiggsAsura <kenneth.bjerke@gmail.com>
2022-12-02 12:18:52 +0100

    button

---
+++ fagskolen/dataelektronikk/school_cpp/leksjon2-samling/button.cpp
@@ -0,0 +1,28 @@
+// consts
+const int buttonPin = 2;
+const int ledPin	= 13;
+
+// variables
+int buttonState		= 0;	// variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbuton pin as an input
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+  
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH
+  if (buttonState == HIGH) {
+    // turn LED on
+    digitalWrite(ledPin, HIGH);
+  }
+//  else if (buttonState == LOW) {
+    // turn LED off
+//    digitalWrite(ledPin, LOW);
+//  }
+}
