b6d260239b310e6fc6276752db8890e90b67e9dc
DiggsAsura <kenneth.bjerke@gmail.com>
2022-12-12 00:14:05 +0100

    p1-defining-an-enum/

--- hello_rust/00-experiment-the-rust-book/6-enums-and-pattern-matching/1-defining-an-enum/p1-defining-an-enum/target/debug/deps/p1_defining_an_enum-24cbd9f1d0271bbf.d
+++
@@ -1,5 +0,0 @@
-/Users/kenneth/github/study/hello_rust/00-experiment-the-rust-book/6-enums-and-pattern-matching/1-defining-an-enum/p1-defining-an-enum/target/debug/deps/p1_defining_an_enum-24cbd9f1d0271bbf.rmeta: src/main.rs
-
-/Users/kenneth/github/study/hello_rust/00-experiment-the-rust-book/6-enums-and-pattern-matching/1-defining-an-enum/p1-defining-an-enum/target/debug/deps/p1_defining_an_enum-24cbd9f1d0271bbf.d: src/main.rs
-
-src/main.rs:
