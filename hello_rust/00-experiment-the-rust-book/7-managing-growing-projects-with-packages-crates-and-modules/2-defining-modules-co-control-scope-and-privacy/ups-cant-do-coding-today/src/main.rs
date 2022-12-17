4a96ed4cd4296e61fdd7ca2135f66199baba1c5b
DiggsAsura <kenneth.bjerke@gmail.com>
2022-12-17 18:17:04 +0100

    going party

---
+++ hello_rust/00-experiment-the-rust-book/7-managing-growing-projects-with-packages-crates-and-modules/2-defining-modules-co-control-scope-and-privacy/ups-cant-do-coding-today/src/main.rs
@@ -0,0 +1,11 @@
+// ok so i been spending 2 days on getting this to work. upgrading nvim to 0.9 and getting all of
+// primeagens stuff up running. Finally looks like it works after maaany attempts!
+// only thnig - now i gonna go party and get drunk. so i will not be able to fix any bugs.
+
+fn main() {
+    let my_vec = vec!["hello", "world"];
+    for i in my_vec {
+        println!("{}", i);
+    }
+}
+
