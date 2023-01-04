55979d91723dc37a308394ace0a69a1082e7d1d7
DiggsAsura <kenneth.bjerke@gmail.com>
2023-01-04 10:40:20 +0100

    12-1

--- hello_rust/00-experiment-the-rust-book/12-an-io-project--building-a-command-line-program/minigrep/src/main.rs
+++ hello_rust/00-experiment-the-rust-book/12-an-io-project--building-a-command-line-program/minigrep/src/main.rs
@@ -1,9 +1,21 @@
 use std::env;   // 12-1
 
 fn main() {
-    // 12-1 --------------------------------------------
+    // Reading the Argument Values
+    // ----------------------------------------------
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    //dbg!(args);
     // 12-1: Collecting the command line arguments into a vector and printing them.
-    // ------------------------------------------------
+    // ----------------------------------------------
+
+    // Saving the Argument Values in Variables
+    // ----------------------------------------------
+    let query = &args[1];       // 1 because 0 is the program itself (minigrep)
+    let file_path = &args[2];   // 2 because 1 is the query
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+    // 12-2: Creating variables to hold the query argument and file path argument
+    // ----------------------------------------------
 }
+
