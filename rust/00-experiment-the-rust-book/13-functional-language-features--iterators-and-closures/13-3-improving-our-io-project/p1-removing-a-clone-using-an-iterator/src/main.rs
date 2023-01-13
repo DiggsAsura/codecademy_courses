323d9f5e38055980f09a706b615bc08d67811262
DiggsAsura <kenneth.bjerke@gmail.com>
2023-01-13 10:06:08 +0100

    p1 using the returend iterator directly

--- rust/00-experiment-the-rust-book/13-functional-language-features--iterators-and-closures/13-3-improving-our-io-project/p1-removing-a-clone-using-an-iterator/src/main.rs
+++ rust/00-experiment-the-rust-book/13-functional-language-features--iterators-and-closures/13-3-improving-our-io-project/p1-removing-a-clone-using-an-iterator/src/main.rs
@@ -43,4 +43,61 @@ impl Config {
 // borrow, we can move the String values from the iterator into Config rather than calling clone
 // and makeing a new allocation.
 //
-/
+//
+// Using the Returned Iterator Directly
+// -----------------------------------
+//
+// Open your I/O project's src/main.rs file, which should look like this:
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments:{err}");
+        process::exit(1);
+    });
+
+    // --snip--
+}
+
+// We'll first change the start of the main function that we had in Listing 12-24 to the code in
+// Listing 13-18, which this time uses an iterator. This on't compile until we update Config::build
+// as well.
+
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    // --snip--
+}
+// 13-18: Passing the return value of env::args to Config::build
+
+// The env::args function returns an iterator! Rather than collecting the iterator values into a
+// vector and then passing a slice to Config::build, now we're passing ownership of the iterator
+// returned from env::args to Config::build directly.
+//
+// Next, we need to update the definition of Config::build. In your I/O project's src/lib.rs file,
+// let's change the signature of Config::build to look like Listing 13-19. This still won't compile
+// because we need to update the function body.
+
+// Filename: src/lib.rs
+impl Config {
+    pub fn build(args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // --snip--
+    }
+}
+// 13-19: Updating the signature of Config::build to expect an iterator
+
+// The standard library documentation for the env::args function shows that the type of iterator it
+// returns is std::env::Args, and that type implements the Iterator trait and returns String
+// values.
+//
+// We've updated the signatures of the Config::build function so that parameter args has generic
+// type with the trait bounds impl Iterator<Item = String> instead of &[String]. This usage of the
+// impl Trait syntax we discussed in the "Traits as Parameters" section of Chapter 10 means that
+// args can be any type that implements the Iterator type and returns String items.
+//
+// Because we're taking ownership of args and we'll be mutating args by iterating over it, we can
+// add the mut keyword into the specification of the args parameter to make it mutable.
