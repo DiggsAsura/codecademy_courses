32663c59df918026572fe0814e64a196b0c4f688
DiggsAsura <kenneth.bjerke@gmail.com>
2023-01-13 10:12:19 +0100

    p1 using iterator trait methods instead of indexing

--- rust/00-experiment-the-rust-book/13-functional-language-features--iterators-and-closures/13-3-improving-our-io-project/p1-removing-a-clone-using-an-iterator/src/main.rs
+++ rust/00-experiment-the-rust-book/13-functional-language-features--iterators-and-closures/13-3-improving-our-io-project/p1-removing-a-clone-using-an-iterator/src/main.rs
@@ -101,3 +101,46 @@ impl Config {
 //
 // Because we're taking ownership of args and we'll be mutating args by iterating over it, we can
 // add the mut keyword into the specification of the args parameter to make it mutable.
+//
+//
+//
+// Using Iterator Trait Methods Instead of Indexing
+// ------------------------------------------------
+//
+// Next, we'll fix the body of Config::build. Because args implements the Iterator trait, we know
+// we can call the next method on it! Listing 13-20 updates the code from Listing 12-23 to use the
+// next method:
+//
+// Filename: src/lib.rs
+
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
+}
+// 13-20: Changing the body of Config::build to use iterator methods
+
+// Remember that the first value in the return value of env::args is the name of the program. We
+// want to ignore that and get to the next value, so first we call next and do nothing with the
+// return value. Second, we call next to get the value we want to put in the query field of Config.
+// If next returns a Some, we use a match to extract the value. If it returns None, it means not
+// enough arguments were given and we return early with an Err value. We do the same thing for the
+// filename value.
