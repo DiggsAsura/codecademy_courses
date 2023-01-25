18204ff445f6e7f291e1e590d08c17e5abc8fe4f
DiggsAsura <kenneth.bjerke@gmail.com>
2023-01-25 09:20:30 +0100

    p3-depending-on-an-external-package-in-a-workspace.md and mods to add

--- rust/00-experiment-the-rust-book/14-more-about-cargo-and-cratesio/14-3-cargo-workspaces/add/adder/src/main.rs
+++ rust/00-experiment-the-rust-book/14-more-about-cargo-and-cratesio/14-3-cargo-workspaces/add/adder/src/main.rs
@@ -1,7 +1,9 @@
 use add_one;
+use rand::Rng;
 
 fn main() {
-    let num = 10;
+    let mut rng = rand::thread_rng();
+    let num: i32 = rng.gen_range(1..101);
     println!("Hello world! {num} plus one is {}!", add_one::add_one(num));
 }
 
