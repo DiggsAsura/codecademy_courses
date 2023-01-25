28851fac4d3076250ae911c71471aff479fcaeb3
DiggsAsura <kenneth.bjerke@gmail.com>
2023-01-25 09:36:54 +0100

    add_two added to add

--- rust/00-experiment-the-rust-book/14-more-about-cargo-and-cratesio/14-3-cargo-workspaces/add/adder/src/main.rs
+++ rust/00-experiment-the-rust-book/14-more-about-cargo-and-cratesio/14-3-cargo-workspaces/add/adder/src/main.rs
@@ -1,9 +1,11 @@
 use add_one;
+use add_two;
 use rand::Rng;
 
 fn main() {
     let mut rng = rand::thread_rng();
     let num: i32 = rng.gen_range(1..101);
     println!("Hello world! {num} plus one is {}!", add_one::add_one(num));
+    println!("Hello world! {num} plus two is {}!", add_two::add_two(num));
 }
 
