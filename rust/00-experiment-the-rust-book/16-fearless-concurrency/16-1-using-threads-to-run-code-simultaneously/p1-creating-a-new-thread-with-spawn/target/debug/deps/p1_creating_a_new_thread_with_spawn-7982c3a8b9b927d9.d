abab451075a8096176548e250a4d2f21dbff3e90
DiggsAsura <kenneth.bjerke@gmail.com>
2023-02-13 23:11:48 +0100

    cleanup

--- rust/00-experiment-the-rust-book/16-fearless-concurrency/16-1-using-threads-to-run-code-simultaneously/p1-creating-a-new-thread-with-spawn/target/debug/deps/p1_creating_a_new_thread_with_spawn-7982c3a8b9b927d9.d
+++
@@ -1,5 +0,0 @@
-/Users/kenneth/github/study/rust/00-experiment-the-rust-book/16-fearless-concurrency/16-1-using-threads-to-run-code-simultaneously/p1-creating-a-new-thread-with-spawn/target/debug/deps/p1_creating_a_new_thread_with_spawn-7982c3a8b9b927d9.rmeta: src/main.rs
-
-/Users/kenneth/github/study/rust/00-experiment-the-rust-book/16-fearless-concurrency/16-1-using-threads-to-run-code-simultaneously/p1-creating-a-new-thread-with-spawn/target/debug/deps/p1_creating_a_new_thread_with_spawn-7982c3a8b9b927d9.d: src/main.rs
-
-src/main.rs:
