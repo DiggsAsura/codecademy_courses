39d810bf2fc9d873b2a12a03456cc1774d49ff09
DiggsAsura <kenneth.bjerke@gmail.com>
2023-02-27 22:52:15 +0100

    quiz1

--- rust/The-Rust-Book/16-fearless-concurrency/16-3-shared-state-concurrency/quiz1/src/main.rs
+++ rust/The-Rust-Book/16-fearless-concurrency/16-3-shared-state-concurrency/quiz1/src/main.rs
@@ -8,3 +8,13 @@ fn main() {
         data.push(0);
     }
 }
+
+
+// Quiz:
+// Which of the following best describes why Rust uses Mutex<T> instead of just Mutex?
+//
+// To prevent accessing a mutex's data without locking the mutex
+//
+//
+// Context: The Mutex<T> design ensures that a mutex's data can only be accessed when the mutex is
+// locked, and conversely that the mutex is unlocked once the data is no longer accessible.
