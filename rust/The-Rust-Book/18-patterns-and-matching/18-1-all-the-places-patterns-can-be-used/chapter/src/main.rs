4a66f6245eabba63409f525e9b2bfbbd6e09b6b2
DiggsAsura <kenneth.bjerke@gmail.com>
2023-03-15 00:45:10 +0100

    p3: while let conditional loops

--- rust/The-Rust-Book/18-patterns-and-matching/18-1-all-the-places-patterns-can-be-used/chapter/src/main.rs
+++ rust/The-Rust-Book/18-patterns-and-matching/18-1-all-the-places-patterns-can-be-used/chapter/src/main.rs
@@ -105,7 +105,36 @@ fn ex2() {
 // whereas with match expressions it does. If we omitted the last else block and therefor missed
 // handling some cases, the compiler would not alert us to the possible logic bug.
 
+
+// while let Conditional Loops
+// ---------------------------
+//
+// Similar in construction to if let, the while let conditional loop allows a while loop to run for
+// as long as a pattern continues to match. In Listing 18-2 we code a while let loop that uses a
+// vector as a stack and prints the values in the vector in the opposite order in which they were
+// pushed.
+
+fn ex3() {
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+}
+// Listing 18-2: Using a while let loop to print values for as long as stack.pop() returns Some
+
+// This example prints 3, 2, 1. The pop method takes the last element out of the vector and returns
+// Some(value). If the vector is empty, pop returns None. The while loop continues running the code
+// in its block as long as pop returns Some. When pop returns None, the loop stops. We can use
+// while let to pop every element off our stack.
+
+
 fn main() {
     ex1();
     ex2();
+    ex3();
 }
