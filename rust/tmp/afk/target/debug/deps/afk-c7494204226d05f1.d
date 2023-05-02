055056ceb1679d4da62d42573ab1c1e70bb232b3
Kenneth <kenneth.bjerke@gmail.com>
2023-05-02 23:24:09 +0200

    clean afk

--- rust/tmp/afk/target/debug/deps/afk-c7494204226d05f1.d
+++
@@ -1,5 +0,0 @@
-/home/kenneth/github/study/rust/tmp/afk/target/debug/deps/afk-c7494204226d05f1.rmeta: src/main.rs
-
-/home/kenneth/github/study/rust/tmp/afk/target/debug/deps/afk-c7494204226d05f1.d: src/main.rs
-
-src/main.rs:
