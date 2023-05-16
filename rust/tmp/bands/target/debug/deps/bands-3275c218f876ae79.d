0a36bc1cf7d65c96c2d28dc3143a8082dabbe57f
DiggsAsura <kenneth.bjerke@gmail.com>
2023-05-17 00:30:48 +0200

    trainingdiary/

---
+++ rust/tmp/bands/target/debug/deps/bands-3275c218f876ae79.d
@@ -0,0 +1,5 @@
+/Users/kenneth/github/study/rust/tmp/bands/target/debug/deps/bands-3275c218f876ae79.rmeta: src/main.rs
+
+/Users/kenneth/github/study/rust/tmp/bands/target/debug/deps/bands-3275c218f876ae79.d: src/main.rs
+
+src/main.rs:
---
+++ rust/tmp/bands/target/debug/deps/bands-36c507bc70937382.d
@@ -0,0 +1,5 @@
+/Users/kenneth/github/study/rust/tmp/bands/target/debug/deps/bands-36c507bc70937382.rmeta: src/main.rs
+
+/Users/kenneth/github/study/rust/tmp/bands/target/debug/deps/bands-36c507bc70937382.d: src/main.rs
+
+src/main.rs:
