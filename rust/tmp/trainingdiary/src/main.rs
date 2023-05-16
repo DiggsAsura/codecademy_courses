409e058e93c7eefc546337f6fc4e1eed8289f19b
DiggsAsura <kenneth.bjerke@gmail.com>
2023-05-16 09:19:05 +0200

    updated program to take input from user, add the entry to a file whih will not overwrite

--- rust/tmp/trainingdiary/src/main.rs
+++ rust/tmp/trainingdiary/src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::fs::OpenOptions;
+
 struct Event {
     day: u8,
     month: u8,
@@ -15,13 +20,44 @@ impl Summary for Event {
     }
 }
 
-fn main() {
+fn main() -> io::Result<()> {
+    // Taking new info from user
+    println!("Enter the day of the event: ");
+    let mut day = String::new();
+    io::stdin().read_line(&mut day).expect("Failed to read line");
+
+    println!("Enter the month of the event: ");
+    let mut month = String::new();
+    io::stdin().read_line(&mut month).expect("Failed to read line");
+
+    println!("Enter the year of the event: ");
+    let mut year = String::new();
+    io::stdin().read_line(&mut year).expect("Failed to read line");
+
+    println!("Enter the description of the event: ");
+    let mut description = String::new();
+    io::stdin().read_line(&mut description).expect("Failed to read line");
+
     let new_event = Event {
-        day: 15,
-        month: 5,
-        year: 2023,
-        description: String::from("Running 6km. No strength training today though. Destroyed after two last days."),
+        day: day.trim().parse().unwrap(),
+        month: month.trim().parse().unwrap(),
+        year: year.trim().parse().unwrap(),
+        description: description.trim().to_string(),
     };
 
-    println!("{}", new_event.summarize());
+ //   println!("{}", new_event.summarize());
+
+    // Make a new file to store the Event
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("diary.txt")
+        .unwrap();
+
+    let entry = new_event.summarize();
+
+    // Write the Event to the file!
+    file.write_all(entry.as_bytes());
+
+    Ok(())
 }
