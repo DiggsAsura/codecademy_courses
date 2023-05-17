82a44bfe196f2b26eddede5ed82dc106d13314c8
DiggsAsura <kenneth.bjerke@gmail.com>
2023-05-17 08:29:39 +0200

    testing enums again

---
+++ rust/tmp/enums/target/debug/deps/enums-69de4d70ece08f7d.d
@@ -0,0 +1,5 @@
+/Users/kenneth/github/study/rust/tmp/enums/target/debug/deps/enums-69de4d70ece08f7d.rmeta: src/main.rs
+
+/Users/kenneth/github/study/rust/tmp/enums/target/debug/deps/enums-69de4d70ece08f7d.d: src/main.rs
+
+src/main.rs:
---
+++ rust/tmp/enumsagain/src/main.rs
@@ -0,0 +1,52 @@
+enum Job {
+    Warrior,
+    Monk,
+    Thief,
+    RedMage,
+    WhiteMage,
+    BlackMage,
+}
+
+impl Job {
+    fn get_name(&self) -> &str {
+        match self {
+            Job::Warrior => "Warrior",
+            Job::Monk => "Monk",
+            Job::Thief => "Thief",
+            Job::RedMage => "Red Mage",
+            Job::WhiteMage => "White Mage",
+            Job::BlackMage => "Black Mage",
+        }
+    }
+}
+
+struct Character {
+    name: String,
+    job: Job,
+    level: u8,
+    hp: u16,
+    mp: u16,
+    strength: u8,
+    dexterity: u8,
+    intelligence: u8,
+    mind: u8,
+    luck: u8,
+}
+
+fn main() {
+    let mut player1 = Character {
+        name: String::from("Cloud"),
+        job: Job::Warrior,
+        level: 1,
+        hp: 100,
+        mp: 0,
+        strength: 10,
+        dexterity: 10,
+        intelligence: 10,
+        mind: 10,
+        luck: 10,
+    };
+
+    println!("{} is a {}.", player1.name, player1.job.get_name());
+}
+
