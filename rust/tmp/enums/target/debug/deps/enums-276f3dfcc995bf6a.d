6fdd232d8f39b35ca152c1f87bb1a766e7fb0b43
DiggsAsura <kenneth.bjerke@gmail.com>
2023-05-17 08:30:03 +0200

    cleanup

--- rust/tmp/enums/target/debug/deps/enums-276f3dfcc995bf6a.d
+++
@@ -1,5 +0,0 @@
-/Users/kenneth/github/study/rust/tmp/enums/target/debug/deps/enums-276f3dfcc995bf6a.rmeta: src/main.rs
-
-/Users/kenneth/github/study/rust/tmp/enums/target/debug/deps/enums-276f3dfcc995bf6a.d: src/main.rs
-
-src/main.rs:
--- rust/tmp/enums/target/debug/deps/enums-69de4d70ece08f7d.d
+++
@@ -1,5 +0,0 @@
-/Users/kenneth/github/study/rust/tmp/enums/target/debug/deps/enums-69de4d70ece08f7d.rmeta: src/main.rs
-
-/Users/kenneth/github/study/rust/tmp/enums/target/debug/deps/enums-69de4d70ece08f7d.d: src/main.rs
-
-src/main.rs:
