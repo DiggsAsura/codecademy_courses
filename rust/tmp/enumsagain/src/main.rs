2817aeb874f814030a58ffb04b93e0655cf5fcc5
DiggsAsura <kenneth.bjerke@gmail.com>
2023-05-21 10:58:34 +0200

    enumsagain

--- rust/tmp/enumsagain/src/main.rs
+++ rust/tmp/enumsagain/src/main.rs
@@ -33,6 +33,18 @@ struct Character {
     luck: u8,
 }
 
+struct Monster {
+    name: String,
+    level: u8,
+    hp: u16,
+    mp: u16,
+    strength: u8,
+    dexterity: u8,
+    intelligence: u8,
+    mind: u8,
+    luck: u8,
+}
+
 fn main() {
     let mut player1 = Character {
         name: String::from("Cloud"),
@@ -47,6 +59,21 @@ fn main() {
         luck: 10,
     };
 
+    let mut monster1 = Monster {
+        name: String::from("Goblin"),
+        level: 1,
+        hp: 100,
+        mp: 0,
+        strength: 10,
+        dexterity: 10,
+        intelligence: 10,
+        mind: 10,
+        luck: 10,
+    };
+
     println!("{} is a {}.", player1.name, player1.job.get_name());
+    println!("Level {}.\n", player1.level);
+
+    println!("{} is a {}.", monster1.name, monster1.level);
 }
 
