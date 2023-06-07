58d12479cc25a98e2661821ae8c2cfd082b8e02f
DiggsAsura <kenneth.bjerke@gmail.com>
2023-06-07 20:17:40 +0200

    what

---
+++ rust/tmp/crapaton/target/debug/deps/crapaton-57b6cc1ad31c7769.d
@@ -0,0 +1,5 @@
+/home/kenneth/github/study/rust/tmp/crapaton/target/debug/deps/crapaton-57b6cc1ad31c7769.rmeta: src/main.rs
+
+/home/kenneth/github/study/rust/tmp/crapaton/target/debug/deps/crapaton-57b6cc1ad31c7769.d: src/main.rs
+
+src/main.rs:
---
+++ rust/tmp/crapaton/target/debug/deps/crapaton-a9ac6cc519bae499.d
@@ -0,0 +1,5 @@
+/home/kenneth/github/study/rust/tmp/crapaton/target/debug/deps/crapaton-a9ac6cc519bae499.rmeta: src/main.rs
+
+/home/kenneth/github/study/rust/tmp/crapaton/target/debug/deps/crapaton-a9ac6cc519bae499.d: src/main.rs
+
+src/main.rs:
