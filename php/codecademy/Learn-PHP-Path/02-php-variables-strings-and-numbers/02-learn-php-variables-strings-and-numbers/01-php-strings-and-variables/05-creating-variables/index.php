80a25f2b59343be9087637bffc8fc996495a51de
DiggsAsura <kenneth.bjerke@gmail.com>
2023-06-09 21:06:14 +0200

    variable, convention

--- php/codecademy/Learn-PHP-Path/02-php-variables-strings-and-numbers/02-learn-php-variables-strings-and-numbers/01-php-strings-and-variables/05-creating-variables/index.php
+++ php/codecademy/Learn-PHP-Path/02-php-variables-strings-and-numbers/02-learn-php-variables-strings-and-numbers/01-php-strings-and-variables/05-creating-variables/index.php
@@ -4,3 +4,19 @@ $my_age = 39;
 
 echo "My name is " . $my_name . " and I am " . $my_age . " years old.";
 echo "\n";
+
+// We are actually doing two things when creating a variable:
+// 1. We are declaring a new variable by giving it a name $my_name
+// 2. Then we are assigning it a value "Kenny B"
+//
+// $:           Sigil
+// my_name:     Variable name
+// =:           Assignment operator
+// "Kenny B":   Variable value
+// ;:           End of statement
+
+// Variable names is case senstive and can not start with a number.
+
+// Convention for variable names is snake_case
+// this_is_snake_case
+
