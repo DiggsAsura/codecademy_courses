6cf7b5c7bf789761e1310a716ec688344b0f60e7
DiggsAsura <kenneth.bjerke@gmail.com>
2023-06-10 22:45:30 +0200

    index.php

---
+++ php/codecademy/Learn-PHP-Path/02-php-variables-strings-and-numbers/02-learn-php-variables-strings-and-numbers/01-php-strings-and-variables/08-variable-reassignment/index.php
@@ -0,0 +1,13 @@
+<?php
+$movie = "___";
+$movie = "The Matrix";
+
+$old_favorite = $movie;
+
+echo "I'm a fickle person, my favorite movie used to be $movie.\n";
+
+$movie = "Insterstellar";
+
+echo "But now my favorite is $movie.\n";
+
+echo "But I will always have a special place in my heart for $old_favorite.";
