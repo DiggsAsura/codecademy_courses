f9b47f34f318350035b2b764d2d611ee26786deb
DiggsAsura <kenneth.bjerke@gmail.com>
2023-06-11 00:00:14 +0200

    concatenation operator

---
+++ php/codecademy/Learn-PHP-Path/02-php-variables-strings-and-numbers/02-learn-php-variables-strings-and-numbers/01-php-strings-and-variables/09-concatenating-assignment-operator/index.php
@@ -0,0 +1,16 @@
+<?php
+// Shorthand for concatenation and parsing
+
+$full_name = "Kenneth";
+$full_name .= " " . "Bæver Bjerke";
+
+echo $full_name . "\n";
+
+
+$sentence = "I like to learn programming. ";
+$sentence .= "I like to learn PHP. ";
+$sentence .= "I like to learn Laravel, maybe some day too";
+
+echo $sentence . "\n";
+
+
