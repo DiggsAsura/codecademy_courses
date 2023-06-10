7eb0436ab7da0cf169fcbfcb03fe21d7ef5d0063
DiggsAsura <kenneth.bjerke@gmail.com>
2023-06-10 15:41:49 +0200

    index.php

---
+++ php/codecademy/Learn-PHP-Path/02-php-variables-strings-and-numbers/02-learn-php-variables-strings-and-numbers/01-php-strings-and-variables/07-variable-parsing/index.php
@@ -0,0 +1,9 @@
+<?php
+$noun = "river";
+$adjective = "sweet";
+$verb = "run";
+
+echo "The world|s most beloved $noun was very $adjective and love to $verb every single day.";
+
+echo "\nI have always been obessed with ${noun}s. I'm ${adjective}ish. I'm always ${verb}ing.\n";
+
