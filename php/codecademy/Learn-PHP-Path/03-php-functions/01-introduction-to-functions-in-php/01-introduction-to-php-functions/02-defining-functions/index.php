7198eef0f6125676f3595740606899ce910e584a
DiggsAsura <kenneth.bjerke@gmail.com>
2023-06-12 00:53:01 +0200

    defining functions

---
+++ php/codecademy/Learn-PHP-Path/03-php-functions/01-introduction-to-functions-in-php/01-introduction-to-php-functions/02-defining-functions/index.php
@@ -0,0 +1,13 @@
+<?php
+function greetLearner() {
+    echo "Hello, Learner!\n";
+    echo "I hope you're enjoying PHP!\n";
+    echo "Love, Codecademy\n\n";
+}
+
+greetLearner();
+
+// camelCase for function names in PHP convention
+
+// typically starts with a verb to describe what the function does
+
