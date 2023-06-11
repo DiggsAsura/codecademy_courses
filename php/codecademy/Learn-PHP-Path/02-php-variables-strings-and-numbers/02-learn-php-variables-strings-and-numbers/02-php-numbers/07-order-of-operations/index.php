c9f6fbb5cd0f9676138a6011c809f8336f0d079a
DiggsAsura <kenneth.bjerke@gmail.com>
2023-06-11 22:19:41 +0200

    order of operations

---
+++ php/codecademy/Learn-PHP-Path/02-php-variables-strings-and-numbers/02-learn-php-variables-strings-and-numbers/02-php-numbers/07-order-of-operations/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * 1. Any operation wrapped in parantheses ()
+ * 2. Exponents (**)
+ * 3. Multiplication (*) and division (/)
+ * 4. Addition (+) and subtration (-)
+ */
+
+echo 2 + 3 * 4; // 14
+echo "\n";
+echo (2 + 3) * 4; // 20
+echo "\n";
+
+echo 94 - 4.25 + 7 - (23.5 * 1.2) + (20 / 4) . "\n";
