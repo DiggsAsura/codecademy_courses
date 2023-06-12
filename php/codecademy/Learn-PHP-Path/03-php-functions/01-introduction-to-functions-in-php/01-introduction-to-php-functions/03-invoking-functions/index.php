1243ee251e53be56fcc6ee4030340da0c185a998
DiggsAsura <kenneth.bjerke@gmail.com>
2023-06-13 00:08:30 +0200

    invoke or call the function

---
+++ php/codecademy/Learn-PHP-Path/03-php-functions/01-introduction-to-functions-in-php/01-introduction-to-php-functions/03-invoking-functions/index.php
@@ -0,0 +1,12 @@
+<?php
+// To invoke the functions just means to call them
+
+function greetLearner() {
+    echo "Hello, Learner!\n";
+    echo "I hope you're (still) enjoying PHP!\n";
+    echo "Love, Codecademy\n";
+}
+
+greetLearner();
+
+
