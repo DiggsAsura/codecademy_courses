614201bc7ab7a79d0beca2e3a9d0414eca5f5f3d
DiggsAsura <kenneth.bjerke@gmail.com>
2023-06-16 01:10:20 +0200

    09-multiple-parameters/

---
+++ php/codecademy/Learn-PHP-Path/03-php-functions/01-introduction-to-functions-in-php/01-introduction-to-php-functions/09-multiple-parameters/index.php
@@ -0,0 +1,15 @@
+<?php
+function calculateArea($height, $width) {
+    return $height * $width;
+}
+
+echo calculateArea(5, 10); // Outputs: 50
+echo "\n";
+
+
+function calculateVolume($height, $width, $depth) {
+    return $height * $width * $depth;
+}
+
+echo calculateVolume(2, 3, 4); // Outputs: 24
+echo "\n";
