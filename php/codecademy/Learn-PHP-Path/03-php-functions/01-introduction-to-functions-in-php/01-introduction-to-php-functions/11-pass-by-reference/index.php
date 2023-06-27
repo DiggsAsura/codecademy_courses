9a654a7147abcea375552d99cfa4062ae370ccb0
DiggsAsura <kenneth.bjerke@gmail.com>
2023-06-27 20:41:32 +0200

    pass by reference

--- php/codecademy/Learn-PHP-Path/03-php-functions/01-introduction-to-functions-in-php/01-introduction-to-php-functions/11-pass-by-reference/index.php
+++ php/codecademy/Learn-PHP-Path/03-php-functions/01-introduction-to-functions-in-php/01-introduction-to-php-functions/11-pass-by-reference/index.php
@@ -35,3 +35,31 @@ fn main() {
     println!("{}", word);
 }
 */
+
+//
+// If we DO want to make permanent changes to a variable within a function,
+// we can prepend the parameter name with the reference sign (&). In this way,
+// we assign the parameter to be an alias for the argument variable. Both will
+// refer to the same spot in memory, and changes to the parameter within the
+// function will permanently affect the argument variable.
+//
+
+
+// the actual task
+
+$string_one = "you have teeth";
+$string_two = "toads are nice";
+$string_three = "brown is my favorite color";
+
+function convertToQuestion(&$str) {
+    $str = "Do you think " . $str . "?\n";
+}
+
+convertToQuestion($string_one);
+convertToQuestion($string_two);
+convertToQuestion($string_three);
+
+echo $string_one;
+echo $string_two;
+echo $string_three;
+
