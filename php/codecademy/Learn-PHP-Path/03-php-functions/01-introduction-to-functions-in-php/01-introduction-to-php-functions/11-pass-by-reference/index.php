846b7a44e3ceeaf6fa961497a419e1476dbfdfce
DiggsAsura <kenneth.bjerke@gmail.com>
2023-06-27 20:23:36 +0200

    testing php vs rust things

--- php/codecademy/Learn-PHP-Path/03-php-functions/01-introduction-to-functions-in-php/01-introduction-to-php-functions/11-pass-by-reference/index.php
+++ php/codecademy/Learn-PHP-Path/03-php-functions/01-introduction-to-functions-in-php/01-introduction-to-php-functions/11-pass-by-reference/index.php
@@ -21,3 +21,17 @@ echo $word . "\n"; // now it's "HelloX"
 
 
 // Is this the exact opposite of Rust?
+// ok so it does in fact do the same thing in Rust.
+/*
+//translate the functionaddXPernamently(&param)into Rust
+fn add_x_permanently(param: &mut String) {
+    param.push_str("X\n");
+    println!("{}", param);
+}
+
+fn main() {
+    let mut word = String::from("Hello");
+    add_x_permanently(&mut word);
+    println!("{}", word);
+}
+*/
