93579a07d02260a6b0200ebfd68cb233356806ce
DiggsAsura <kenneth.bjerke@gmail.com>
2023-11-27 13:42:34 +0100

    testing concurrency

---
+++ rust_v2/conc4/src/main.rs
@@ -0,0 +1,25 @@
+// a simple app with two blinking dots on the screen, but in two separate threads. cli app.
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    // handle.join().unwrap(); // this will wait for the spawned thread to finish before continuing
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap(); // this will wait for the spawned thread to finish before continuing
+}
+
+
+
